8e281491a4dea00aa2492c96908f248dfbe7da87
roosterduck <roosterduck@disroot.org>
2025-10-26 00:42:52 +0800

    update the script to compile busybox

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -12,7 +12,16 @@ OSTREPO=$(realpath ./rootfs/ostree/repo)
 
 echo repository=$REPO > $TARGET/etc/xbps.d/00-repository.conf
 sudo xbps-install -Syu --rootdir "$TARGET"
-sudo xbps-install -y --rootdir "$TARGET" busybox xbps linux linux-headers dinit ostree
+sudo xbps-install -y --rootdir "$TARGET" xbps linux linux-headers dinit ostree
+
+git clone https://github.com/mirror/busybox.git --depth 1
+cd busybox
+make defconfig
+sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/' .config
+sed -i 's/CONFIG_TC=y/CONFIG_TC=n/' .config
+make -j$(nproc)
+make install CONFIG_PREFIX="$TARGET"
+
 echo "# placeholder" > "$TARGET"/etc/fstab
 echo "Rootfs built at $TARGET"
 
